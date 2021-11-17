34b296b9fe834546ffd5453a516bb9318e6159fd
Omar38 <omar.sultanov.98@mail.ru>
2021-11-17 23:26:24 +0300

    Updated filesgit  - espress js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const fn=function(){
-    console.log("Created environment");
-}
+const express=require("express")
+const app=express()
+const port=3000;
+
+app.get("/",(req,res)=>{
+    res.send("HELLO WORLD")
+})
+app.listen(port,()=>{
+    console.log(`OUR SSEVER IS STARTING ${port}`);
+})
