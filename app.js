e453b960f1c1e303de550c2a9aaea884ab0dfae8
Omar38 <omar.sultanov.98@mail.ru>
2021-11-18 15:54:17 +0300

    Add static files and ejs engine

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 const port = 3000;
-
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve((__dirname, 'temp/index.html')));
+  //   res.sendFile(path.resolve((__dirname, 'temp/index.html')));
+  res.render('index');
 });
-
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 app.listen(port, () => {
   console.log(`OUR SSEVER IS STARTING ${port}`);
 });
